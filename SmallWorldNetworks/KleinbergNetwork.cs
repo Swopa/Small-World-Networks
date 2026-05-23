7741f516e4e84ca9fe4e677e4dcf596666c47c5d
kiu_tepnadze.nikoloz <nikoloz.tephnadze@tutanota.com>
2026-05-23 19:42:41 +0400

    Implemented Kleinberg Model

--- SmallWorldNetworks/KleinbergNetwork.cs
+++ SmallWorldNetworks/KleinbergNetwork.cs
@@ -1,12 +1,124 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Diagnostics.CodeAnalysis;
 
 namespace SmallWorldNetworks
 {
     public class KleinbergNetwork
     {
+        public int n; //We need nxn matrix
+        public int p; //lattice distance of local neighbors\
+        public int q;
+        public double r;
+        public int nodeCount; //number of all nodes
+        public List<int>[] adjacencyList;
+        Random random;
+
+        public KleinbergNetwork(int n, int p, int q, int r)
+        {
+            this.n = n;
+            this.r = r;
+            this.p = p;
+            this.q = q;
+            nodeCount = n * n;
+            adjacencyList = new List<int>[nodeCount];
+            random = new Random();
+            Construct();
+        }
+
+        private void Construct() 
+        {
+            for(int i = 0; i < nodeCount; i++) 
+            {
+                adjacencyList[i] = new List<int>();
+            }
+
+            for (int i = 0; i < nodeCount; i++) 
+            {
+                for (int j = 0; j < nodeCount; j++) 
+                {
+                    if (i != j && GetLatticeDistance(i, j) <= p) 
+                    {
+                        adjacencyList[i].Add(j);
+                    }
+                }
+            }
+
+            for (int i = 0; i < nodeCount; i++)
+            {
+                AddLongRangeContacts(i);
+            }
+        }
+
+        private void AddLongRangeContacts(int index) 
+        {
+            int[] distances = new int[nodeCount];
+            double[] weights = new double[nodeCount];
+
+            for (int i = 0; i < nodeCount; i++) 
+            {
+                distances[i] = GetLatticeDistance(index, i); 
+            }
+
+            for (int i = 0; i < nodeCount; i++)
+            {
+                if (distances[i] != 0) weights[i] = 1 / Math.Pow(distances[i], r);
+            }
+
+            double sum = 0;
+
+            foreach(var weight in weights) sum += weight;
+
+            int picked = 0;
+            int attempts = 0;
+            
+            while (picked < q) 
+            {
+                
+                attempts++;
+                if (attempts > 10000) { Console.WriteLine("Stuck!"); break; }
+                double rand = random.NextDouble() * sum;
+                double acc = 0;
+                for (int i = 0; i < nodeCount; i++) 
+                {
+                    if (i == index) continue;
+                    acc += weights[i];
+                    if (acc > rand)
+                    {
+                        if (!adjacencyList[index].Contains(i)) 
+                        {
+                            adjacencyList[index].Add(i);
+                            picked++;
+                        }
+                        break;
+                    }
+                         
+                }
+            }
+        }
+
+        private int GetIndex(int row, int col) 
+        { 
+            return row * n + col;
+        }
+
+        private int GetRow(int index) 
+        {
+            return index / n;
+        }
+
+        private int GetCol(int index) 
+        { 
+            return index % n;
+        }
+
+        private int GetLatticeDistance(int index1, int index2) 
+        {
+            int x1 = GetRow(index1);
+            int x2 = GetRow(index2);
+            int y1 = GetCol(index1);
+            int y2 = GetCol(index2);
+
+            return Math.Abs(x1 - x2) + Math.Abs(y1 - y2);
+        }
+
     }
 }
--- SmallWorldNetworks/Program.cs
+++ SmallWorldNetworks/Program.cs
@@ -1,20 +1,39 @@
 ﻿using SmallWorldNetworks;
 
-WattsStrogatzNetwork ws = new WattsStrogatzNetwork(1000, 6, 1, true);
+//WattsStrogatzNetwork ws = new WattsStrogatzNetwork(1000, 6, 1, true);
 
-for (int i = 0; i < ws.adjacencyList.Length; i++)
+//for (int i = 0; i < ws.adjacencyList.Length; i++)
+//{
+//    Console.Write($"Node {i} has neighbors: ");
+//    for (int j = 0; j < ws.adjacencyList[i].Count; j++)
+//    {
+//        Console.Write($"{ws.adjacencyList[i][j]} ");
+//    }
+//    Console.WriteLine("");
+//}
+
+//Console.WriteLine($"average path length: {NetworkMetrics.CalculateAveragePathLength(ws.adjacencyList)}");
+
+//Console.WriteLine($"average clustering coefficient: {NetworkMetrics.CountClusteringCoefficient(ws.adjacencyList)}");
+
+KleinbergNetwork k = new KleinbergNetwork(20, 2, 1, 2);
+
+for (int i = 0; i < k.adjacencyList.Length; i++) 
 {
-    Console.Write($"Node {i} has neighbors: ");
-    for (int j = 0; j < ws.adjacencyList[i].Count; j++)
+    Console.Write($"Node {i/20}x{i%20} has neighbors: ");
+    for (int j = 0; j < k.adjacencyList[i].Count; j++)
     {
-        Console.Write($"{ws.adjacencyList[i][j]} ");
+        Console.Write($"{k.adjacencyList[i][j]/20}x{k.adjacencyList[i][j] % 20} ");
     }
     Console.WriteLine("");
 }
 
-Console.WriteLine($"average path length: {NetworkMetrics.CalculateAveragePathLength(ws.adjacencyList)}");
+Console.WriteLine($"average path length: {NetworkMetrics.CalculateAveragePathLength(k.adjacencyList)}");
 
-Console.WriteLine($"average clustering coefficient: {NetworkMetrics.CountClusteringCoefficient(ws.adjacencyList)}");
+Console.WriteLine($"average clustering coefficient: {NetworkMetrics.CountClusteringCoefficient(k.adjacencyList)}");
+
+
+
 
 
 
